9bf0e3018cd6d048874721edfa5cdbac07181c8b
Prince Kumar <dhimanprince262@gmail.com>
2026-02-24 04:30:42 +0530

    added framer-motion

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LatestJobCard from './LatestJobCard';
 import { useSelector } from 'react-redux';
 import useGetAllJobs from '@/hooks/useGetAllJobs';
+import { motion } from 'framer-motion';
 
 const LatestJobs = () => {
     useGetAllJobs();
@@ -15,7 +16,23 @@ const LatestJobs = () => {
 
             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
-                    allJobs?.length <= 0 ? <span className='text-xl text-gray-600 m-auto'>Refresh the page</span> : allJobs?.slice(0, 6).map((job) => <LatestJobCard key={job?._id} job={job}/>)
+                    allJobs?.length <= 0 ?
+                        < motion.span
+                            initial={{ opacity: 0, x: 100 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 0.3 }}
+                            className='text-2xl text-gray-600 m-auto'>Loading...</motion.span> :
+                        allJobs?.slice(0, 6).map((job) => (
+                            <motion.div key={job?._id}
+                                initial={{ opacity: 0, x: 100 }}
+                                animate={{ opacity: 1, x: 0 }}
+                                exit={{ opacity: 0, x: -100 }}
+                                transition={{ duration: 0.3 }} >
+
+                                <LatestJobCard job={job} />
+                            </motion.div>
+                        ))
                 }
             </div>
         </div>
